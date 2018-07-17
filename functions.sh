c7c33d381332a4fca30b812334a32656a32cc0f0
Omri <omrilotan@users.noreply.github.com>
2018-07-17 10:37:07 +0100

    convert indentation to tabs

--- functions.sh
+++ functions.sh
@@ -1,31 +1,31 @@
 function tab() {
-  TAB_NAME=$1
-  COMMAND=$2
-  osascript \
-    -e "tell application \"Terminal\"" \
-    -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
-    -e "do script \"printf '\\\e]1;$TAB_NAME\\\a'; $COMMAND\" in front window" \
-    -e "end tell" > /dev/null
+	TAB_NAME=$1
+	COMMAND=$2
+	osascript \
+		-e "tell application \"Terminal\"" \
+		-e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
+		-e "do script \"printf '\\\e]1;$TAB_NAME\\\a'; $COMMAND\" in front window" \
+		-e "end tell" > /dev/null
 }
 
 function name {
-  [ $# -eq 0 ] && title="$(pwd | rev | cut -d/ -f1 | rev)" || title="$*"
-  echo -ne "\033]0;"$title"\007"
+	[ $# -eq 0 ] && title="$(pwd | rev | cut -d/ -f1 | rev)" || title="$*"
+	echo -ne "\033]0;"$title"\007"
 }
 
 function dir {
-  mkdir "$1"
-  cd "$1"
+	mkdir "$1"
+	cd "$1"
 }
 
 function google {
-  open "https://www.google.com/?q=$*"
+	open "https://www.google.com/?q=$*"
 }
 
 function sitespeed {
 	if [ -z "$1" ]; then
-    echo "Please enter a URL"
-  else
-    docker run --shm-size=1g --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io-autobuild:latest $1
+		echo "Please enter a URL"
+	else
+		docker run --shm-size=1g --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io-autobuild:latest $1
 	fi
 }
--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ dots+=(custom)
 CHECK_MARK="\033[0;32m\xE2\x9C\x94\033[0m"
 for dot in "${dots[@]}"
 do
-  echo -n "⏳ Processing $dot"
+	echo -n "⏳ Processing $dot"
 	[[ -s "$HOME/.dotfiles/$dot.sh" ]] && source "$HOME/.dotfiles/$dot.sh"
 	echo -e "\\r${CHECK_MARK} $dot finished   "
 done

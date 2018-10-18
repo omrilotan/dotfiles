c5d79376919f1ca2f38fa3789c2e9ba91e2fbf1a
Omri Lotan <omrilotan@users.noreply.github.com>
2018-10-18 14:46:34 +0300

    Bash you say?

--- Programs/bash.sh
+++ Programs/bash.sh
@@ -1,2 +1,4 @@
-bind 'set completion-ignore-case on'
-[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+if [[ $(basename $SHELL) == "bash" ]]; then
+	bind 'set completion-ignore-case on'
+	[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+fi

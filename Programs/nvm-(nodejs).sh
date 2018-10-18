ea178834a863e7ce474465a3ada1033501d20d02
Omri Lotan <omrilotan@users.noreply.github.com>
2018-10-18 09:28:28 +0300

    Use NVM only, load for all node commands

--- Programs/nvm-(nodejs).sh
+++ Programs/nvm-(nodejs).sh
@@ -1,10 +1,24 @@
-function nvm {
+# Remove native installed nodejs
+# sudo rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/{npm*,node*,man1/node*}
+# brew uninstall --force node
+
+nodecommands=(nvm npm npx node)
+function nvm_load {
+	# Unset all node command decorators
+	for nodecommand in "${nodecommands[@]}"; do
+		eval "unset -f ${nodecommand}"
+	done
+	unset -f nvm_load
 	if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
+		echo "Loading NVM"
 		source "$HOME/.nvm/nvm.sh"
-		eval "nvm $@"
 	else
 		echo "Can't find nvm script"
 	fi
 }
+# All node commands should load NVM first
+for nodecommand in "${nodecommands[@]}"; do
+	eval "function ${nodecommand} { nvm_load; ${nodecommand} \$@; }"
+done
 
 export NPMRC=$(cat ~/.npmrc) # For docker images etc.

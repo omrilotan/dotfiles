7a168dbbddedd8c8d9d67a8655113c609c8dd905
Omri Lotan <omrilotan@users.noreply.github.com>
2019-01-10 11:38:14 +0200

    🗄 bin dir in home

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 [[ -s "$HOME/dotfiles/.gitconfig" ]]    || ln -s ~/.gitconfig ~/dotfiles/.gitconfig
 [[ -s "$HOME/dotfiles/hyper.js" ]]      || rm ~/.hyper.js
 [[ -s "$HOME/dotfiles/hyper.js" ]]      || ln -s ~/dotfiles/hyper.js ~/.hyper.js
+[[ -d "$HOME/dotfiles/.bin" ]] || [[ -d "$HOME/bin" ]] && ln -s ~/bin ~/dotfiles/.bin
 
 [[ -z $(cat ~/.bash_profile | grep "dotfiles/init.sh") ]] && echo "source ~/dotfiles/init.sh" >> .bash_profile

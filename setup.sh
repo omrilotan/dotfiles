4ab611a137f5abfaed207366ad10fed25bd10101
Omri Lotan <omrilotan@users.noreply.github.com>
2019-02-12 14:19:09 +0200

    symlink hosts file

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 [[ -s "$HOME/dotfiles/hyper.js" ]]      || rm ~/.hyper.js
 [[ -s "$HOME/dotfiles/hyper.js" ]]      || ln -s ~/dotfiles/hyper.js ~/.hyper.js
 [[ -d "$HOME/dotfiles/.bin" ]] || [[ -d "$HOME/bin" ]] && ln -s ~/bin ~/dotfiles/.bin
+[[ -d "$HOME/dotfiles/hosts" ]]         || ln -s /etc/hosts ~/dotfiles/hosts
 
 [[ -z $(cat ~/.bash_profile | grep "dotfiles/init.sh") ]] && echo "source ~/dotfiles/init.sh" >> .bash_profile

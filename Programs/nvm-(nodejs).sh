ac3c342c479f92bf71c3bcbc3e2218b918e20ba0
Omri Lotan <omrilotan@users.noreply.github.com>
2019-09-15 21:37:58 +0300

    "Get that shit outta my master."

--- Programs/nvm-(nodejs).sh
+++ Programs/nvm-(nodejs).sh
@@ -2,28 +2,34 @@
 # sudo rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/{npm*,node*,man1/node*}
 # brew uninstall --force node
 
-nodecommands=(nvm npm npx node)
-function nvm_load {
-	# Unset all node command decorators
+if [ -f ".nvmrc" ]; then
+	# Load NVM if needed
+	source "$HOME/.nvm/nvm.sh"
+else
+	# Set up idem potent NVM loaders
+	nodecommands=(nvm npm npx node)
+	function nvm_load {
+		# Unset all node command decorators
+		for nodecommand in "${nodecommands[@]}"; do
+			eval "unset -f ${nodecommand}"
+		done
+		unset -f nvm_load
+		if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
+			START=$(date +%s)
+			echo "Loading NVM"
+			source "$HOME/.nvm/nvm.sh"
+			END=$(date +%s)
+			DIFF=$(echo "($END - $START)" | bc)
+			echo -e "\033[0;94mLoading NVM took ${DIFF} seconds\033[0m"
+		else
+			echo "Can't find nvm script"
+		fi
+	}
+	# All node commands should load NVM first
 	for nodecommand in "${nodecommands[@]}"; do
-		eval "unset -f ${nodecommand}"
+		eval "function ${nodecommand} { nvm_load; ${nodecommand} \$@; }"
 	done
-	unset -f nvm_load
-	if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
-		START=$(date +%s)
-		echo "Loading NVM"
-		source "$HOME/.nvm/nvm.sh"
-		END=$(date +%s)
-		DIFF=$(echo "($END - $START)" | bc)
-		echo -e "\033[0;94mLoading NVM took ${DIFF} seconds\033[0m"
-	else
-		echo "Can't find nvm script"
-	fi
-}
-# All node commands should load NVM first
-for nodecommand in "${nodecommands[@]}"; do
-	eval "function ${nodecommand} { nvm_load; ${nodecommand} \$@; }"
-done
+fi
 
 export NPMRC=$(cat ~/.npmrc) # For docker images etc.
 

688c500e9aaae62f1a4821b60cac9383b5e09d6b
Omri Lotan <omrilotan@users.noreply.github.com>
2019-10-02 22:25:37 +0300

    a more programatic setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,38 @@
-[[ -s "$HOME/dotfiles/.bash_profile" ]] || ln -s ~/.bash_profile ~/dotfiles/.bash_profile
-[[ -s "$HOME/dotfiles/.gitconfig" ]]    || ln -s ~/.gitconfig ~/dotfiles/.gitconfig
-[[ -s "$HOME/dotfiles/hyper.js" ]]      || rm ~/.hyper.js
-[[ -s "$HOME/dotfiles/hyper.js" ]]      || ln -s ~/dotfiles/hyper.js ~/.hyper.js
-[[ -d "$HOME/dotfiles/.bin" ]] || [[ -d "$HOME/bin" ]] && ln -s ~/bin ~/dotfiles/.bin
-[[ -s "$HOME/dotfiles/hosts" ]]         || ln -s /etc/hosts ~/dotfiles/hosts
-[[ -s "$HOME/dotfiles/shells" ]]        || ln -s /etc/shells ~/dotfiles/shells
-[[ -s "$HOME/dotfiles/.npmrc" ]]        || ln -s ~/.npmrc ~/dotfiles/.npmrc
+#!/bin/bash
 
-[[ -z $(cat ~/.bash_profile | grep "dotfiles/init.sh") ]] && echo "source ~/dotfiles/init.sh" >> .bash_profile
+# Symlinks
+q=()
+q+=('("Bash profile" .bash_profile ~/.bash_profile)')
+q+=('("Git config" .gitconfig ~/.gitconfig)')
+q+=('("Hyper config" .hyper.js ~/.hyper.js)')
+q+=('("Hosts file" hosts /etc/hosts)')
+q+=('("Acceptable shells" shells /etc/shells)')
+q+=('("Hyper config" .npmrc ~/.npmrc)')
+q+=('("Sublime Settings" sublime-settings "~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings")')
+
+for set in "${q[@]}";
+do
+	eval "parts=${set}"
+
+	if [[ ! -s "$HOME/dotfiles/${parts[1]}" ]]; then
+		ln -s "${parts[2]}" "${parts[1]}"
+		echo "created symlink for ${parts[0]}"
+	fi
+done
+
+# Personal bin
+if [[ ! -d "$HOME/dotfiles/.bin" ]]; then
+	if [[ -d "$HOME/bin" ]]; then
+		ln -s ~/bin ~/dotfiles/.bin
+		echo "Created .bin symlink"
+	fi
+fi
+
+# Node completion
+node --completion-bash > ~/dotfiles/Programs/node-complete.sh
+
+# This library's init
+if [[ -z $(cat ~/.bash_profile | grep "dotfiles/init.sh") ]]; then
+	echo "source ~/dotfiles/init.sh" >> .bash_profile
+	echo "Added call to init to bash_profile"
+fi

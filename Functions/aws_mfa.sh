1824a4567f85e38be3cdf8c2c5f97875c6678178
Omri Lotan <omrilotan@users.noreply.github.com>
2020-01-03 00:03:23 +0200

    Not sure why

--- Functions/aws_mfa.sh
+++ Functions/aws_mfa.sh
@@ -1,4 +1,19 @@
 function aws_mfa {
+	if [[ "$1" = "--help" || "$1" = "-h" ]]; then
+		echo -e "
+aws_mfa: sign in with aws multi factor authentication
+‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+
+aws_mfa [profile] [token]
+
+> Examples:
+$ aws_mfa my.name 345345
+$ aws_mfa my.name
+$ aws_mfa
+"
+		return
+	fi
+
 	local profile=$1
 	local token=$2
 

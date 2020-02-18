d927f448cbffe4dd80b6b4b2642adec5647146e8
omrilotan <omri@hamadgera.com>
2020-02-18 10:55:10 +0200

    Add my sublime settings

--- Programs/nvm-(nodejs).sh
+++ Programs/nvm-(nodejs).sh
@@ -10,6 +10,11 @@ function load_nvm {
 	source ~/dotfiles/.npm_completion.sh
 }
 
+export NVM_DIR="$HOME/.nvm"
+# [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+# [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+
 if [ -f ".nvmrc" ]; then
 	# Load NVM if needed
 	load_nvm

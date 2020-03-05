3a47d89efba210093c1dad51b23657a9301fdfa5
omrilotan <omri@hamadgera.com>
2020-03-05 08:59:21 +0200

    should work I guess...

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ q+=('("Hyper config" .hyper.js ~/.hyper.js)')
 q+=('("Hosts file" hosts /etc/hosts)')
 q+=('("Acceptable shells" shells /etc/shells)')
 q+=('("Hyper config" .npmrc ~/.npmrc)')
-q+=('("Sublime Settings" sublime-settings "~/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings")')
 q+=('("Terminalizer config" .terminalizer.yml ~/.terminalizer/config.yml)')
 q+=('("Missing file" missing ~/.missing)')
 

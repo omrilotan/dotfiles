3b5484e5f8ab039aaa2e2ce7234169532d8451bb
omrilotan <omrilotan@hotmail.com>
2025-03-31 15:09:14 +0100

    stuff

--- Functions/clean.sh
+++ Functions/clean.sh
@@ -2,9 +2,7 @@ function clean {
 	if [ "$1" == "npx" ]; then
 		echo "Cleaning npx cache"
 		rm -rf ~/.npm/_npx
-		exit 0
 	fi
 
-	echo "Please provide a valid argument"
-	exit 1
+	echo "Please provide a valid argument (npx)"
 }
--- General/editors.sh
+++ General/editors.sh
@@ -3,7 +3,8 @@ function subedit {
 	sub /Users/omri\.lotan/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 }
 alias atom='/Applications/Atom.app/Contents/Resources/app/atom.sh'
-alias vscode='/Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Resources/app/bin/code'
+# alias vscode='/Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Resources/app/bin/code'
+alias vscode='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
 alias github='/Applications/GitHub\ Desktop.app/Contents/Resources/app/static/github.sh open $(pwd)$@'
 
 function code {

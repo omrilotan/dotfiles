dae18e6e3f7b0de302fb763fee4ce94ab4801924
omrilotan <omri@hamadgera.com>
2026-02-23 16:57:06 +0000

    cd functionality

--- Functions/cd.sh
+++ Functions/cd.sh
@@ -1,3 +1,41 @@
+cd() {
+	dir=$@
+	: ${dir:=~}
+
+	if [ -f "$1" ]; then
+		echo "Change to the directory containing $(basename "$1")"
+		dir=$(dirname "$1")
+	fi
+
+	# Change directory
+	builtin cd "$dir" "${@:2}"
+
+	if [ -f ".nvmrc" ]; then
+		echo "Setup nodejs env"
+		nvm use
+	fi
+
+	if [ -f ".ruby-version" ]; then
+		echo "Setup ruby env"
+
+		if [ -f ".ruby-gemset" ]; then
+			rvm use "$(cat .ruby-version)@$(cat .ruby-gemset)"
+		else
+			rvm use $(cat .ruby-version)
+		fi
+	fi
+
+	if [ -f ".python-version" ]; then
+		echo "Using python version $(cat .python-version) with uv"
+		uv venv
+	fi
+
+	if [ -d ".venv" ]; then
+		echo "Activate python venv"
+		source .venv/bin/activate
+	fi
+}
+
 function cd? {
 	if [[ "$1" = "--help" || "$1" = "-h" ]]; then
 		echo -e "
@@ -19,9 +57,9 @@ $ cd? modules
 	fi
 	set -- $(ls -a | grep $q --fixed-strings)
 	if [ ! -z $1 ]; then
-		# echo "Change Directory to $1"
-		cd $1
+		echo "Change directory to $1"
+		builtin cd $1
 		return 0
 	fi
-	# echo "Did not find $q"
+	echo "Did not find directory $q"
 }
